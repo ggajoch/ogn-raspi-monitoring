e9d92e81731c3a8fb19da4fb7ad7410ac6af7e79
Grzegorz Gajoch <gajoch@gmail.com>
2020-07-01 03:12:45 +0200

    Update raspi_deamon.py

--- raspi_deamon.py
+++ raspi_deamon.py
@@ -1,7 +1,7 @@
 import os
 import serial
 import time
-import urllib.request
+import urllib
 from time import sleep
 
 baseURL = 'http://api.thingspeak.com/update?api_key=ELO0PS5JA8RX73B5&field1={}&field2={}&field3={}'
@@ -13,24 +13,36 @@ def measure_temp():
 
 
 while True:
-    ser = serial.Serial('/dev/ttyUSB0', 9600)
+    while True:
+        for i in range(10):
+            try:
+                ser = serial.Serial('/dev/ttyUSB0', 9600)
+            except Exception as e:
+                print(e)
+                sleep(1)
+            else:
+                break
+
+        if ser and ser.is_open:
+            break
 
     while True:
         try:
-            line = ser.readline()
-            arduino_temp, volts = line.strip().split()
-
-            volts = float(volts)
-            arduino_temp = float(arduino_temp)
-
-            raspi_temp = measure_temp()
+            for _ in range(20):
+                line = ser.readline()
+                arduino_temp, volts = line.strip().split()
+                volts = float(volts)
+                arduino_temp = float(arduino_temp)
+                raspi_temp = measure_temp()
+                
             print(volts, arduino_temp, raspi_temp)
 
             s = baseURL.format(volts, arduino_temp, raspi_temp)
-            f = urllib.request.urlopen(s)
+            f = urllib.urlopen(s)
             f.read()
             f.close()
         except Exception as e:
             print(e)
+            sleep(1)
 
     sleep(10)
